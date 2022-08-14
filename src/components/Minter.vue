df04e27c05a62b3958a857ca636b05a70ff95242
Callum Buttery <callumbuttery@icloud.com>
2022-08-14 16:44:05 +0100

    Feat: add drop box for minting

--- src/components/Minter.vue
+++ src/components/Minter.vue
@@ -4,7 +4,16 @@
         <div class="minterSecondaryContainer">
             <v-row justify="center" class="mintButtonContainer">
                 <v-col justify="center" align="center">
-                    <v-btn color="red" class="white--text minterButton" justify="center">Begin Minting</v-btn>
+                    <v-btn color="red" class="white--text minterButton" id="beginMintingButton" justify="center"
+                        @click="displayMinting()">Begin Minting</v-btn>
+                    <div id="actualMintingContainer">
+                        <v-combobox dense filled hide-selected outlined solo class="comboBox" :items="items"
+                            placeholder="1 NFT">
+                        </v-combobox>
+                        <v-row justify="center" align="center">
+                            <v-btn id="actualMintingButton">Mint</v-btn>
+                        </v-row>
+                    </div>
                 </v-col>
             </v-row>
         </div>
@@ -13,13 +22,42 @@
 </template>
 
 <script>
+
 export default {
     name: 'Minter',
+    data() {
+        return {
+            items: [
+                '1 NFT',
+                '2 NFTs',
+                '3 NFTs',
+                '4 NFTs',
+                '5 NFTs',
+            ],
+        }
+    },
+
+    methods: {
+        displayMinting() {
+            document.getElementById('beginMintingButton').style.display = "none";
+            document.getElementById('actualMintingContainer').style.display = "block";
+            document.getElementById('actualMintingButton').style.display = "block";
+
+        }
+    },
+    mounted() {
+        document.getElementById('actualMintingContainer').style.display = "none";
+        document.getElementById('actualMintingButton').style.display = "none";
+    }
 }
 
 </script>
 
 <style>
+.comboBox {
+    width: 100px;
+}
+
 .minterPimraryContainer {
     padding-top: 100px;
     background-color: #FEF1BE;
@@ -44,6 +82,13 @@ export default {
     padding-left: 40%;
 }
 
+
+.actualMintingContainer {
+    padding-top: 400px;
+    padding-left: 40%;
+}
+
+
 @media only screen and (max-width: 2500px) {
     .mintButtonContainer {
         padding-top: 800px;
@@ -52,6 +97,11 @@ export default {
 
     .minterPimraryContainer {
         height: 1200px;
+    }
+
+    .actualMintingContainer {
+        padding-top: 800px;
+        padding-left: 40%;
     }
 }
 
@@ -63,6 +113,11 @@ export default {
 
     .minterPimraryContainer {
         height: 800px;
+    }
+
+    .actualMintingContainer {
+        padding-top: 500px;
+        padding-left: 30%;
     }
 }
 
@@ -76,6 +131,12 @@ export default {
     .minterPimraryContainer {
         height: 500px;
     }
+
+    .actualMintingContainer {
+        padding-top: 250px;
+        padding-left: 35%;
+    }
+
 }
 
 @media screen and (max-width: 1020px) {
@@ -86,6 +147,11 @@ export default {
 
     .minterPimraryContainer {
         height: 400px;
+    }
+
+    .actualMintingContainer {
+        padding-top: 200px;
+        padding-left: 35%;
     }
 }
 
@@ -100,6 +166,10 @@ export default {
         height: 300px;
         background-image: none;
     }
-}
 
+    .actualMintingContainer {
+        padding-top: 0px;
+        padding-left: 0%;
+    }
+}
 </style>
