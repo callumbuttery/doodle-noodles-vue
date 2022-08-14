cf80bc452bd7add963aaebb666f77d9b89010d23
Callum Buttery <callumbuttery@icloud.com>
2022-08-14 16:01:51 +0100

    Feat: minter with mobile responsiveness

---
+++ src/components/Minter.vue
@@ -0,0 +1,82 @@
+<template>
+
+    <div class="minterPimraryContainer">
+        <div class="minterSecondaryContainer">
+            <v-row justify="center" class="mintButtonContainer">
+                <v-col justify="center" align="center">
+                    <v-btn color="red" class="white--text minterButton" justify="center">Begin Minting</v-btn>
+                </v-col>
+            </v-row>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Minter',
+}
+
+</script>
+
+<style>
+.minterPimraryContainer {
+    padding-top: 100px;
+    background-color: #FEF1BE;
+    background-image: url("../assets/imgs/MINT.png");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-position-y: 100%;
+    background-size: 100%;
+    height: 700px;
+    max-width: 60%;
+    margin: 0 auto;
+    aspect-ratio: auto;
+}
+
+
+.v-application--wrap {
+    background-color: #FEF1BE;
+}
+
+.mintButtonContainer {
+    padding-top: 400px;
+    padding-left: 40%;
+}
+
+@media screen and (max-width: 1600px) {
+    .mintButtonContainer {
+        padding-top: 250px;
+        padding-left: 35%;
+    }
+
+    .minterPimraryContainer {
+        height: 500px;
+    }
+}
+
+@media screen and (max-width: 1020px) {
+    .mintButtonContainer {
+        padding-top: 200px;
+        padding-left: 35%;
+    }
+
+    .minterPimraryContainer {
+        height: 400px;
+    }
+}
+
+
+@media screen and (max-width: 768px) {
+    .mintButtonContainer {
+        padding-top: 0px;
+        padding-left: 0%;
+    }
+
+    .minterPimraryContainer {
+        height: 300px;
+        background-image: none;
+    }
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Carousel/>
+    <Minter/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Carousel from '@/components/Carousel.vue'
+import Minter from '@/components/Minter.vue'
 
 export default {
   name: 'Home',
   components: {
-    Carousel
+    Carousel,
+    Minter,
   }
 }
 </script>
