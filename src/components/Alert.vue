5bee6086d27fd146f7470039a6ab6912d50c500e
Callum Buttery <callumbuttery@icloud.com>
2022-08-15 21:37:22 +0100

    Feat: add alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-alert elevation="13" :type="type">{{$store.state.alertMessage}}</v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Alert',
+    props: {
+        type: {
+            type: String,
+        }
+    },
+    watchers: {
+        type(newType) {
+            this.type = newType;
+        }
+    },
+}
+
+</script>
